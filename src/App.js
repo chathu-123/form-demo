b5033b07e5b92cfb0ad1d46e668f974f2aa4fc16
Chathuni W <chathuni2002@gmail.com>
2024-07-30 13:55:16 +1200

    initialize code

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [contact, setContact] = useState('');
+  const [submit, setSubmit] = useState('');
+  const handleSubmit=(e) => {
+    e.preventDefault();
+    console.log('Clicked');
+    setName(submit);
+    setEmail(submit);
+    setContact(submit);
+  }
+
+  return (
+    <div style={{textAlign:'center'}}>
+      
+      <form onSubmit={handleSubmit}>
+      <h1>Name</h1>
+      <input
+      type="text"
+      placeholder="Your name"
+      onChange={(e) => setSubmit(e.target.value)}
+      />
+      <h1>Email</h1>
+      <input
+      type="text"
+      placeholder="Your email"
+      onChange={(e) => setSubmit(e.target.value)}
+      />
+      <h1>Contact No</h1>
+      <input
+      type="text"
+      placeholder="Your contact no"
+      onChange={(e) => setSubmit(e.target.value)}
+      />
+      <br></br><br></br>
+      <button type="submit">Submit</button>
+      </form>
+      <h2>Hello, {name} your email is {email} your contact no is {contact}</h2>
+    </div>
+  );
+}
+
+export default App;
